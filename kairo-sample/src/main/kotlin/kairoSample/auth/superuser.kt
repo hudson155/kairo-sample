cbfce9e7015e581d20a3e48394352f7ac3817d15
Jeff Hudson <jhudson@jhudson.ca>
2024-10-21 23:55:28 -0600

    Use MissingJwtClaim exception in auth

--- kairo-sample/src/main/kotlin/kairoSample/auth/superuser.kt
+++ kairo-sample/src/main/kotlin/kairoSample/auth/superuser.kt
@@ -2,13 +2,15 @@ package kairoSample.auth
 
 import kairo.rest.auth.Auth
 import kairo.rest.auth.JwtPrincipal
+import kairo.rest.exception.MissingJwtClaim
 import kairo.rest.exception.NoPrincipal
 import kairoSample.exception.NotSuperuser
 
 internal fun Auth.superuser(): Auth.Result {
   val principal = principal ?: return Auth.Result.Exception(NoPrincipal())
   val jwt: JwtPrincipal.() -> Auth.Result = principal@{
-    val platformRolesClaim = getClaim<List<PlatformRole>>(JwtClaimName.platformRoles).orEmpty()
+    val platformRolesClaim = getClaim<List<PlatformRole>>(JwtClaimName.platformRoles)
+      ?: throw MissingJwtClaim(JwtClaimName.platformRoles)
     if (PlatformRole.Superuser !in platformRolesClaim) return@principal Auth.Result.Exception(NotSuperuser())
     return@principal Auth.Result.Success
   }
--- kairo-sample/src/main/kotlin/kairoSample/auth/userAccount.kt
+++ kairo-sample/src/main/kotlin/kairoSample/auth/userAccount.kt
@@ -4,6 +4,7 @@ import kairo.id.KairoId
 import kairo.rest.auth.Auth
 import kairo.rest.auth.JwtPrincipal
 import kairo.rest.auth.overriddenBy
+import kairo.rest.exception.MissingJwtClaim
 import kairo.rest.exception.NoPrincipal
 import kairoSample.exception.NoAccessToUserAccount
 
@@ -13,7 +14,7 @@ internal fun Auth.userAccount(userAccountId: KairoId): Auth.Result {
   val principal = principal ?: return Auth.Result.Exception(NoPrincipal())
   val jwt: JwtPrincipal.() -> Auth.Result = principal@{
     val userAccountIdClaim = getClaim<KairoId>(JwtClaimName.userAccountId)
-      ?: return@principal Auth.Result.Exception(NoAccessToUserAccount())
+      ?: throw MissingJwtClaim(JwtClaimName.userAccountId)
     if (userAccountIdClaim != userAccountId) return@principal Auth.Result.Exception(NoAccessToUserAccount())
     return@principal Auth.Result.Success
   }
