2e679e497eeb8c3f4e81fd77a50750b0ae5887af
Jeff Hudson <jhudson@jhudson.ca>
2024-10-22 17:08:00 -0600

    Query update

--- kairo-sample/src/main/resources/store/libraryBook/searchByText.sql
+++ kairo-sample/src/main/resources/store/libraryBook/searchByText.sql
@@ -1,5 +1,5 @@
 select *
 from library.library_book
-where (:title is null or (strpos(lower(title), :title) > 0))
-  and (:author is null or (strpos(lower(author), :author) > 0))
+where (:title is null or (strpos(lower(title), lower(:title)) > 0))
+  and (:author is null or (strpos(lower(author), lower(:author)) > 0))
   and deleted_at is null
