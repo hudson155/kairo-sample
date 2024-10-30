61e3b7e1cd9948486b90020c09e9d40440431710
Jeff Hudson <jhudson@jhudson.ca>
2024-10-30 15:34:22 -0400

    Move classes below members (#12)

--- kairo-sample/src/main/kotlin/kairoSample/common/KairoSampleRep.kt
+++ kairo-sample/src/main/kotlin/kairoSample/common/KairoSampleRep.kt
@@ -3,9 +3,9 @@ package kairoSample.common
 import kairo.id.KairoId
 
 public abstract class KairoSampleRep {
+  public abstract val id: KairoId
+
   public abstract class Creator
 
   public abstract class Update
-
-  public abstract val id: KairoId
 }
