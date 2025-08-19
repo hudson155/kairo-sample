cf40f7fc77a6f0460b5a3640c2f6189883a8d45d
Jeff Hudson <jeff@highbeam.co>
2025-08-19 09:55:56 -0600

    Main

--- src/main/kotlin/kairoSample/Main.kt
+++ src/main/kotlin/kairoSample/Main.kt
@@ -5,18 +5,31 @@ import com.typesafe.config.ConfigFactory
 import kairo.application.kairo
 import kairo.dependencyInjection.DependencyInjectionFeature
 import kairo.healthCheck.feature.HealthCheckFeature
+import kairo.id.IdGenerationStrategy
+import kairo.id.RandomIdGenerationStrategy
 import kairo.rest.RestFeature
 import kairo.server.Server
+import kairoSample.libraryBook.LibraryBookFeature
 import kotlinx.serialization.hocon.Hocon
 import kotlinx.serialization.hocon.decodeFromConfig
 import org.apache.logging.log4j.LogManager
+import org.koin.dsl.module
+import org.koin.ksp.generated.defaultModule
 
 fun main() {
   kairo {
     val config = loadConfig()
     val features = listOf(
-      DependencyInjectionFeature {},
+      DependencyInjectionFeature {
+        modules(
+          defaultModule,
+          module {
+            single<IdGenerationStrategy> { RandomIdGenerationStrategy(length = 22) }
+          },
+        )
+      },
       HealthCheckFeature(),
+      LibraryBookFeature(),
       RestFeature(Hocon.decodeFromConfig(config.getConfig("rest"))),
     )
     val server = Server(
