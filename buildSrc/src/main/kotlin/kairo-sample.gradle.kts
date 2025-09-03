5773db12e4431fcbdec8122675b7e2a50d1245bf
Jeff Hudson <jeff@highbeam.co>
2025-09-03 09:30:58 -0600

    Add missing stuff

--- buildSrc/src/main/kotlin/kairo-sample.gradle.kts
+++ buildSrc/src/main/kotlin/kairo-sample.gradle.kts
@@ -13,6 +13,31 @@ repositories {
   artifactRegistry()
 }
 
+java {
+  toolchain {
+    languageVersion = javaVersion
+  }
+}
+
+kotlin {
+  jvmToolchain {
+    languageVersion = javaVersion
+  }
+  compilerOptions {
+    allWarningsAsErrors = true
+    freeCompilerArgs.add("-Xannotation-default-target=param-property")
+    freeCompilerArgs.add("-Xconsistent-data-class-copy-visibility")
+    freeCompilerArgs.add("-Xcontext-parameters")
+    freeCompilerArgs.add("-Xjsr305=strict")
+    freeCompilerArgs.add("-Xlambdas=indy")
+    freeCompilerArgs.add("-Xnested-type-aliases")
+    freeCompilerArgs.add("-opt-in=kotlin.concurrent.atomics.ExperimentalAtomicApi")
+    freeCompilerArgs.add("-opt-in=kotlin.time.ExperimentalTime")
+    freeCompilerArgs.add("-opt-in=kotlin.uuid.ExperimentalUuidApi")
+    freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
+  }
+}
+
 /**
  * Detekt makes the [check] task depend on the [detekt] task automatically.
  * However, since the [detekt] task doesn't support type resolution
