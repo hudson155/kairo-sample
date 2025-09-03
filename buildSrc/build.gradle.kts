4852d672ed37ddc10f52b2635424070f6d585445
Jeff Hudson <jeff@highbeam.co>
2025-09-03 16:41:44 -0600

    Upgrade Kairo dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,12 +7,12 @@ repositories {
 }
 
 dependencies {
-  val kotlinVersion = "2.2.0" // https://kotlinlang.org/docs/releases.html#release-details
+  val kotlinVersion = "2.2.10" // https://kotlinlang.org/docs/releases.html#release-details
   implementation(kotlin("gradle-plugin", kotlinVersion))
   implementation(kotlin("serialization", kotlinVersion))
 
   // https://github.com/google/ksp/releases
-  implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.2.0-2.0.2")
+  implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.2.10-2.0.2")
 
   // https://plugins.gradle.org/plugin/com.google.cloud.artifactregistry.gradle-plugin
   implementation("com.google.cloud.artifactregistry:artifactregistry-gradle-plugin:2.2.5")
