e61882deabf059f185079e82576d7cad09cdab3b
Jeff Hudson <jeff@highbeam.co>
2025-09-05 08:53:51 -0600

    Fewer deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,18 +30,14 @@ dependencies {
   implementation(platform("io.ktor:ktor-bom:3.2.3"))
   implementation(platform("org.slf4j:slf4j-bom:2.0.17"))
 
-  implementation("io.github.oshai:kotlin-logging-jvm:7.0.13")
   implementation("org.slf4j:slf4j-api")
-  implementation("org.slf4j:slf4j-simple")
-  runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.7.RELEASE")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+  runtimeOnly("org.slf4j:slf4j-simple")
+
   implementation("org.jetbrains.exposed:exposed-core")
-  implementation("org.jetbrains.exposed:exposed-crypt")
-  implementation("org.jetbrains.exposed:exposed-json")
   implementation("org.jetbrains.exposed:exposed-kotlin-datetime")
-  implementation("org.jetbrains.exposed:exposed-money")
   implementation("org.jetbrains.exposed:exposed-r2dbc")
-  implementation("io.r2dbc:r2dbc-pool:1.0.2.RELEASE")
+  runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.7.RELEASE")
+
   implementation("io.ktor:ktor-server-core")
   implementation("io.ktor:ktor-server-call-logging")
   implementation("io.ktor:ktor-server-content-negotiation")
