d4489a23fa779f6a063f3161cf78536d6d039a05
Jeff Hudson <jeff@highbeam.co>
2025-10-30 19:44:38 -0600

    Some deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,19 @@ kotlin {
 
 dependencies {
   implementation(libs.kairo.application)
+  implementation(libs.kairo.config)
+  implementation(libs.kairo.coroutines)
+  implementation(libs.kairo.dependencyInjection.feature)
+  implementation(libs.kairo.healthCheck.feature)
+  implementation(libs.kairo.id)
+  implementation(libs.kairo.logging)
+  implementation(libs.kairo.rest.feature)
+  implementation(libs.kairo.serialization)
+  implementation(libs.kairo.sql.feature)
+  implementation(libs.kairo.util)
+  implementation(libs.log4j.core)
+  runtimeOnly(libs.log4j.json)
+  runtimeOnly(libs.log4j.slf4j)
 }
 
 application {
