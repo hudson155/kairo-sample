7e49db76a62a66058e8a225bbd77639656ab7acd
Jeff Hudson <jeff@highbeam.co>
2025-10-30 21:27:33 -0600

    Base test

---
+++ src/test/kotlin/kairoSample/library/LibraryFeatureTest.kt
@@ -0,0 +1,37 @@
+package kairoSample.library
+
+import kairo.dependencyInjection.DependencyInjectionFeature
+import kairo.feature.FeatureTest
+import kairo.server.Server
+import kairo.sql.PostgresExtensionAware
+import kairo.sql.SqlFeature
+import kairo.sql.from
+import kairoSample.libraryBook.LibraryBookFeature
+import kairoSample.libraryBook.LibraryBookTable
+import org.jetbrains.exposed.v1.core.Schema
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.koin.core.KoinApplication
+
+internal class LibraryFeatureTest : FeatureTest(), PostgresExtensionAware {
+  override fun beforeEach(context: ExtensionContext) {
+    transaction(db = checkNotNull(context.database)) {
+      SchemaUtils.createSchema(Schema("library"))
+      SchemaUtils.create(
+        LibraryBookTable,
+      )
+    }
+    super.beforeEach(context)
+  }
+
+  override fun createServer(context: ExtensionContext, koinApplication: KoinApplication): Server =
+    Server(
+      name = "Library Feature Test Server",
+      features = listOf(
+        DependencyInjectionFeature(koinApplication),
+        LibraryBookFeature(koinApplication.koin),
+        SqlFeature.from(checkNotNull(context.connectionFactory)),
+      ),
+    )
+}
