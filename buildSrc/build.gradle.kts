c473ca3aedb629f7e0cd882ac4c57297321e76c4
Jeff Hudson <jeff@highbeam.co>
2025-11-10 19:02:04 -0700

    Alphabetize buildSrc dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,22 +7,22 @@ repositories {
 }
 
 dependencies {
+  // https://plugins.gradle.org/plugin/com.google.cloud.artifactregistry.gradle-plugin
+  val artifactRegistryVersion = "2.2.5"
+  implementation("com.google.cloud.artifactregistry:artifactregistry-gradle-plugin:$artifactRegistryVersion")
+
+  // https://github.com/detekt/detekt/releases
+  val detektVersion = "2.0.0-alpha.1"
+  implementation("dev.detekt:detekt-gradle-plugin:$detektVersion")
+
   // https://kotlinlang.org/docs/releases.html#release-details
   val kotlinVersion = "2.2.20"
   implementation(kotlin("gradle-plugin", kotlinVersion))
   implementation(kotlin("serialization", kotlinVersion))
 
-  // https://plugins.gradle.org/plugin/com.google.cloud.artifactregistry.gradle-plugin
-  val artifactRegistryVersion = "2.2.5"
-  implementation("com.google.cloud.artifactregistry:artifactregistry-gradle-plugin:$artifactRegistryVersion")
-
   // https://github.com/google/ksp/releases
   val kspVersion = "2.2.20-2.0.3"
   implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:$kspVersion")
-
-  // https://github.com/detekt/detekt/releases
-  val detektVersion = "2.0.0-alpha.1"
-  implementation("dev.detekt:detekt-gradle-plugin:$detektVersion")
 
   // https://github.com/GradleUp/shadow/releases
   val shadowVersion = "8.3.9"
