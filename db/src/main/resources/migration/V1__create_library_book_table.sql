601b941ced88f655a6a47e01b9474337323a1af0
Jeff Hudson <jeffryderhudson@gmail.com>
2025-11-10 23:34:12 -0700

    Write script to generate migrations (#97)

---
+++ db/src/main/resources/migration/V1__create_library_book_table.sql
@@ -0,0 +1,15 @@
+create table if not exists library.library_book
+(
+  id text
+    primary key,
+  created_at timestamp default current_timestamp not null,
+  version bigint default 0 not null,
+  updated_at timestamp default current_timestamp not null,
+  title text null,
+  authors text[] not null,
+  isbn text not null
+);
+
+alter table library.library_book
+  add constraint uq__library_book__isbn
+    unique (isbn);
