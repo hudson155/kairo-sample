af17437404683c4a553b16dd490f01a07a522331
Jeff Hudson <jeff@highbeam.co>
2025-11-10 23:46:14 -0700

    Improve database config

--- db/src/main/kotlin/kairoSample/db/GenerateMigrationScript.kt
+++ db/src/main/kotlin/kairoSample/db/GenerateMigrationScript.kt
@@ -5,13 +5,12 @@ import org.jetbrains.exposed.v1.migration.r2dbc.MigrationUtils
 import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 
 fun main() {
-  val scriptName = requireNotNull(System.getProperty("scriptName"))
   runBlocking {
     suspendTransaction(database) {
       MigrationUtils.generateMigrationScript(
         tables = tables.toTypedArray(),
         scriptDirectory = "src/main/resources/migration",
-        scriptName = scriptName,
+        scriptName = "V0__generated",
       )
     }
   }
