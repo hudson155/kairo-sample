6e19b6d4e4543c9e6b9934a9efe02de4deb8159c
Jeff Hudson <jeff@highbeam.co>
2025-11-11 18:10:56 -0700

    Write script to run migrations

---
+++ db/src/main/kotlin/kairoSample/db/RunMigrations.kt
@@ -0,0 +1,25 @@
+package kairoSample.db
+
+import kairo.protectedString.ProtectedString
+import org.flywaydb.core.Flyway
+
+@OptIn(ProtectedString.Access::class)
+private val flyway: Flyway by lazy {
+  Flyway.configure().apply {
+    dataSource(databaseConfig.url, databaseConfig.username, databaseConfig.password.value)
+    table("database_migration")
+    defaultSchema("public")
+    schemas(
+      "library",
+    )
+    locations("classpath:migration")
+    validateOnMigrate(true)
+    cleanOnValidationError(false)
+    cleanDisabled(true)
+    createSchemas(true)
+  }.load()
+}
+
+fun main() {
+  flyway.migrate()
+}
